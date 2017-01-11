9a4f6cb993444b4b0a4c46d07151f84b46d4693f
Martin <martinbutts@clear.net.nz>
2017-01-12 10:56:57 +1300

    add twitter api keys

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ var twitter = require('twitter')
 var dotenv = require('dotenv')
 
 dotenv.load()
+
+var client = new twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
