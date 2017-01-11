ce8aefaa7a893b6f09c2587ad80412335aada53e
Martin <martinbutts@clear.net.nz>
2017-01-12 11:35:37 +1300

    add to log

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-var twitter = require('twitter')
+var Twitter = require('twitter')
 var dotenv = require('dotenv')
 
 dotenv.load()
 
-var client = new twitter({
+var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
 });
